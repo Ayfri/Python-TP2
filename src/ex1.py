bd201bc434ee73106877b5c29e7beb50e8a71758
Ayfri <pierre.ayfri@gmail.com>
2022-01-28 16:20:16 +0100

    feat: Add documentation to ex1 & 2.

--- src/ex1.py
+++ src/ex1.py
@@ -1,9 +1,19 @@
 from utils.inputs import int_input
 
 def ex1() -> None:
+	"""
+	Écrire un algorithme qui demande successivement 10 nombres à
+	l’utilisateur, et qui lui dise ensuite quel était le plus grand parmi
+	ces 10 nombres.
+	Modifiez ensuite l’algorithme pour que le programme affiche de
+	surcroît en quelle position avait été saisie ce nombre.
+
+	:return: None
+	:rtype: None
+	"""
 	array = [int_input(f"Entrez le nombre n°{i} : ") for i in range(1, 11)]
 	print(f"""Le plus grand de ces nombres est : {max(array)}
-Son index est : {array.index(max(array))}""")
+C'était le numéro {array.index(max(array))}""")
 
 if __name__ == "__main__":
 	ex1()
--- src/ex2.py
+++ src/ex2.py
@@ -3,11 +3,16 @@ import math
 from utils.inputs import int_input
 
 def ex2() -> None:
+	"""
+	Écrire un algorithme qui permette de connaître ses chances de gagner au tiercé, quarté, quinté
+	et autres impôts volontaires.
+	On demande à l’utilisateur le nombre de chevaux partants et le nombre de chevaux joués.
+
+	:return: None
+	:rtype: None
+	"""
 	horses_participating: int = int_input("Combien de chevaux participent : ")
 	horses_played: int = int_input("Combien de chevaux sont joués : ")
-	# ! is factorial
-	# x = n ! / (n - p) !
-	# y = n ! / (p ! * (n – p) !)
 	x = math.factorial(horses_participating) / math.factorial(horses_participating - horses_played)
 	y = math.factorial(horses_participating) / (
 				math.factorial(horses_played) * math.factorial(horses_participating - horses_played))
